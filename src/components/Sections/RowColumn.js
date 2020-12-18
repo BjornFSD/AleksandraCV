213537c25a3dac4fee12bbd843d91be358f872a2
mrmisiex <misiek10061998@gmail.com>
2020-12-18 23:45:30 +0100

    Titles in template3

--- src/components/Sections/RowColumn.js
+++ src/components/Sections/RowColumn.js
@@ -10,16 +10,16 @@ function RowColumn() {
     backgroundColor: "#dfdfdf",
     boxShadow: " -350px 0 0 #dfdfdf",
   };
-
+  // to show titles set title with id
   const boxArr = [
-    { src: "./img/t1.jpg", id: 0 },
-    { src: "./img/t2.jpg", id: 1 },
-    { src: "./img/t3.jpg", id: 2 },
-    { src: "./img/t1.jpg", id: 3 },
-    { src: "./img/t2.jpg", id: 4 },
-    { src: "./img/t3.jpg", id: 5 },
-    { src: "./img/t1.jpg", id: 6 },
-    { src: "./img/t2.jpg", id: 7 },
+    { src: "./img/t1.jpg", id: 0, title: "Something 1" },
+    { src: "./img/t2.jpg", id: 1, title: "2" },
+    { src: "./img/t3.jpg", id: 2, title: "Something 3" },
+    { src: "./img/t1.jpg", id: 3, title: "4" },
+    { src: "./img/t2.jpg", id: 4, title: "Something 5" },
+    { src: "./img/t3.jpg", id: 5, title: "6" },
+    { src: "./img/t1.jpg", id: 6, title: "Something 7" },
+    { src: "./img/t2.jpg", id: 7, title: "8" },
   ];
 
   const splitArr = boxArr.reduce(function (box, key, index) {
@@ -31,6 +31,7 @@ function RowColumn() {
 
   const showImages = splitArr.map((item, index) => {
     // console.log(index % 2, item[1]);
+    let n = 0;
     const ss = item.map((item2, index) => {
       return item2.src === undefined ? null : (
         <img alt="" src={item2.src} id={item2.id} key={index}></img>
@@ -44,14 +45,14 @@ function RowColumn() {
     // console.log(ss.item);
     return !(odd === null) ? (
       <div className="columns_box-titleOdd" key={index}>
-        <p>Something</p>
+        <p>{item[n].title}</p>
         <div className="columns_box-img" style={stylesOdd}>
           {odd}
         </div>
       </div>
     ) : (
       <div className="columns_box-titleEven" key={index}>
-        <p>Something</p>
+        <p>{item[n].title}</p>
         <div className="columns_box-img" style={stylesEven}>
           {even}
         </div>
